e56fa2a46b89c1952c58b08a1413be9f5cd49af3
Garret Bassett <garret.bassett@gmail.com>
2020-04-15 17:18:23 -0600

    Add AlertBox class and related functionality in Main class

--- src/AlertBox.java
+++ src/AlertBox.java
@@ -1,6 +1,32 @@
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
 
 public class AlertBox {
 
-    //TESTING
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label(message);
+        Button closeButton = new Button("CLOSE WINDOW");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+
+
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,8 @@ import java.util.Stack;
 public class Main extends Application {
 
     Stage window;
-    Scene scene1, scene2;
+    Button button;
+    // Scene scene1, scene2; // Use for switching between scenes
 
 
     public static void main(String[] args) {
@@ -26,40 +27,53 @@ public class Main extends Application {
     /**
      * Creating alert boxes
      */
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        window.setTitle("Another title");
 
+        Button button = new Button("Click me please");
+        button.setOnAction(e -> AlertBox.display("AlertBox Title", "Hey, it worked"));
 
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 200, 200);
+        window.setScene(scene);
+        window.show();
+
+    }
 
     /**
      * Switching between scenes
      */
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        window = stage;
-
-        Label label1 = new Label("Welcome to Scene 1.");
-        Button button1 = new Button("Go to Scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
-
-        // Layout 1 - children in column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
-
-        // Button 2
-        Button button2 = new Button("Go back to Scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
-
-        // Layout 2 - children
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 200, 300);
-
-        window.setScene(scene1);
-        window.setTitle("TITLE HERE");
-        window.show();
-
-    }
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        window = stage;
+//
+//        Label label1 = new Label("Welcome to Scene 1.");
+//        Button button1 = new Button("Go to Scene 2");
+//        button1.setOnAction(e -> window.setScene(scene2));
+//
+//        // Layout 1 - children in column
+//        VBox layout1 = new VBox(20);
+//        layout1.getChildren().addAll(label1, button1);
+//        scene1 = new Scene(layout1, 200, 200);
+//
+//        // Button 2
+//        Button button2 = new Button("Go back to Scene 1");
+//        button2.setOnAction(e -> window.setScene(scene1));
+//
+//        // Layout 2 - children
+//        StackPane layout2 = new StackPane();
+//        layout2.getChildren().add(button2);
+//        scene2 = new Scene(layout2, 200, 300);
+//
+//        window.setScene(scene1);
+//        window.setTitle("TITLE HERE");
+//        window.show();
+//
+//    }
 
     /**
      * Creating scenes + buttons
